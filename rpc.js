757518218b8878f885292a55b170e97597a5af76
gaming-cbu <siburamnayak18@.com>
2026-05-17 22:29:26 +0530

    Change The History Disign

--- rpc.js
+++ rpc.js
@@ -11,16 +11,134 @@ let ChoiceImage = document.querySelector("#cir-comp-y");
 
 let aiChoiceImage = document.querySelector("#cir-comp-x");
 
+// History Add Start
+
+let hisMain = document.querySelector(".history");
+
+let box1 = document.querySelector("#round-1");
+let box2 = document.querySelector("#round-2");
+let box3 = document.querySelector("#round-3");
+let box4 = document.querySelector("#round-4");
+let box5 = document.querySelector("#round-5");
+
+
+
+let winColor0 = document.querySelector(".bord-1-his");
+let winOrLoss0 = document.querySelector("#win-or-loss-1");
+let yourImg0 = document.querySelector(".img1-his-you");
+let aiImg0 = document.querySelector(".img-his-ai");
+let scoreColor0 = document.querySelector(".score-color");
+let bordHis0 = document.querySelector(".bord-1-hisX");
+
+
+let winColor1 = document.querySelector(".bord-2-his");
+let winOrLoss1 = document.querySelector("#win-or-loss-2");
+let yourImg1 = document.querySelector(".img1-his-you-2");
+let aiImg1 = document.querySelector(".img-his-ai-2");
+let scoreColor1 = document.querySelector(".score-color-2");
+let bordHis1 = document.querySelector(".bord-2-hisX");
+
+
+let winColor2 = document.querySelector(".bord-3-his");
+let winOrLoss2 = document.querySelector("#win-or-loss-3");
+let yourImg2 = document.querySelector(".img1-his-you-3");
+let aiImg2 = document.querySelector(".img-his-ai-3");
+let scoreColor2 = document.querySelector(".score-color-3");
+let bordHis2 = document.querySelector(".bord-3-hisX");
+
+
+let winColor3 = document.querySelector(".bord-4-his");
+let winOrLoss3 = document.querySelector("#win-or-loss-4");
+let yourImg3 = document.querySelector(".img1-his-you-4");
+let aiImg3 = document.querySelector(".img-his-ai-4");
+let scoreColor3 = document.querySelector(".score-color-4");
+let bordHis3 = document.querySelector(".bord-4-hisX");
+
+
+let winColor4 = document.querySelector(".bord-5-his");
+let winOrLoss4 = document.querySelector("#win-or-loss-5");
+let yourImg4 = document.querySelector(".img1-his-you-5");
+let aiImg4 = document.querySelector(".img-his-ai-5");
+// let scoreColor4 = document.querySelector(".score-color-5");
+let bordHis4 = document.querySelector(".bord-5-hisX");
+
+// History End Here
+
+let buttomText = document.querySelector("#made-button");
+
+
+
 let gameRound = 0;
 let lastRound = 5;
 let win = 0;
 let loss = 0;
+let winLoss = "";
+let countX = -1;
+let hisHight = 0;
 
 const aiClasses = [
     "human-choice-rock",
     "human-choice-paper",
     "human-choice-scissors"
 ];
+
+const winColor = [
+    winColor0,
+    winColor1,
+    winColor2,
+    winColor3,
+    winColor4
+];
+
+
+const winOrLoss = [
+    winOrLoss0,
+    winOrLoss1,
+    winOrLoss2,
+    winOrLoss3,
+    winOrLoss4
+];
+
+const yourImg = [
+    yourImg0,
+    yourImg1,
+    yourImg2,
+    yourImg3,
+    yourImg4
+];
+
+const aiImg = [
+    aiImg0,
+    aiImg1,
+    aiImg2,
+    aiImg3,
+    aiImg4,
+];
+
+// const scoreColor = [
+//     scoreColor0,
+//     scoreColor1,
+//     scoreColor2,
+//     scoreColor3,
+//     scoreColor4
+// ];
+
+const bordHis = [
+    bordHis0,
+    bordHis1,
+    bordHis2,
+    bordHis3,
+    bordHis4,
+];
+
+const box = [
+    box1,
+    box2,
+    box3,
+    box4,
+    box5
+];
+ 
 
 function randomNum() {
     return Math.floor(Math.random() * 3) + 1;
@@ -30,7 +148,7 @@ function checkResult(player, ai) {
 
     if (player === ai) {
         lastRound++;
-        console.log("Draw");
+        winLoss = "draw";
 
     } else if (
         (player === 1 && ai === 3) ||
@@ -39,17 +157,50 @@ function checkResult(player, ai) {
         ) {
 
         win++;
+        countX++;
         humanCount.innerText = win;
-        console.log("Win");
+        winLoss = "win";
+        
 
     } else {
  
         loss++;
+        countX++;
         aiCount.innerText = loss;
-        console.log("Loss");
+        winLoss = "loss";
 
     };
+    console.log(winLoss);
 
+
+    if (winLoss === "win") {
+        hisMain.style.display = "flex";
+
+        winColor[countX].style.backgroundColor = "#00FF9D";
+        winOrLoss[countX].innerText = "WIN";
+        bordHis[countX].style.backgroundColor = "#00FF9D";
+        bordHis[countX].innerText = "+1";
+        box[countX].style.display = "flex";
+
+        hisHight += 53;
+        hisMain.style.height = `${hisHight}px`;
+       
+    } else if (winLoss === "loss") {
+        hisMain.style.display = "flex";
+
+        winColor[countX].style.backgroundColor = "#FF5C7A";
+        winOrLoss[countX].innerText = "LOSS";
+        bordHis[countX].style.backgroundColor = "#FF5C7A";
+        bordHis[countX].innerText = "-1";
+        box[countX].style.display = "flex";
+
+        hisHight += 53;
+        hisMain.style.height = `${hisHight}px`;
+    };
+
+    
+
+ 
 
 
 };
@@ -77,14 +228,29 @@ function playGame(player) {
         ChoiceImage.classList.remove("human-choice-scissors");
         ChoiceImage.classList.remove("human-choice-paper");
         ChoiceImage.classList.add("human-choice-rock");
+
+        yourImg[countX].classList.add("human-choice-rock");
+ 
     } else if (player===2) {
         ChoiceImage.classList.remove("human-choice-rock");
         ChoiceImage.classList.remove("human-choice-scissors");
         ChoiceImage.classList.add("human-choice-paper");
+
+        yourImg[countX].classList.add("human-choice-paper");
     } else {
         ChoiceImage.classList.remove("human-choice-rock");
         ChoiceImage.classList.remove("human-choice-paper");
         ChoiceImage.classList.add("human-choice-scissors");
+
+        yourImg[countX].classList.add("human-choice-scissors");
+    }
+
+    if (ai===1) {
+        aiImg[countX].classList.add("human-choice-rock");
+    } else if (ai===2) {
+        aiImg[countX].classList.add("human-choice-rock");
+    } else {
+        aiImg[countX].classList.add("human-choice-scissors");
     }
 } 
 

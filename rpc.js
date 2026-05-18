fd03073c731b12ce43ff7551d26c1be999c35b9c
gaming-cbu <siburamnayak18@.com>
2026-05-18 21:37:24 +0530

    Nothing...

--- rpc.js
+++ rpc.js
@@ -21,15 +21,12 @@ let box3 = document.querySelector("#round-3");
 let box4 = document.querySelector("#round-4");
 let box5 = document.querySelector("#round-5");
 
-
-
 let winColor0 = document.querySelector(".bord-1-his");
 let winOrLoss0 = document.querySelector("#win-or-loss-1");
 let yourImg0 = document.querySelector(".img1-his-you");
 let aiImg0 = document.querySelector(".img-his-ai");
 let scoreColor0 = document.querySelector(".score-color");
 let bordHis0 = document.querySelector(".bord-1-hisX");
-
 
 let winColor1 = document.querySelector(".bord-2-his");
 let winOrLoss1 = document.querySelector("#win-or-loss-2");
@@ -38,7 +35,6 @@ let aiImg1 = document.querySelector(".img-his-ai-2");
 let scoreColor1 = document.querySelector(".score-color-2");
 let bordHis1 = document.querySelector(".bord-2-hisX");
 
-
 let winColor2 = document.querySelector(".bord-3-his");
 let winOrLoss2 = document.querySelector("#win-or-loss-3");
 let yourImg2 = document.querySelector(".img1-his-you-3");
@@ -46,14 +42,12 @@ let aiImg2 = document.querySelector(".img-his-ai-3");
 let scoreColor2 = document.querySelector(".score-color-3");
 let bordHis2 = document.querySelector(".bord-3-hisX");
 
-
 let winColor3 = document.querySelector(".bord-4-his");
 let winOrLoss3 = document.querySelector("#win-or-loss-4");
 let yourImg3 = document.querySelector(".img1-his-you-4");
 let aiImg3 = document.querySelector(".img-his-ai-4");
 let scoreColor3 = document.querySelector(".score-color-4");
 let bordHis3 = document.querySelector(".bord-4-hisX");
-
 
 let winColor4 = document.querySelector(".bord-5-his");
 let winOrLoss4 = document.querySelector("#win-or-loss-5");
@@ -66,54 +60,26 @@ let bordHis4 = document.querySelector(".bord-5-hisX");
 
 let buttomText = document.querySelector("#made-button");
 
-
-
 let gameRound = 0;
 let lastRound = 5;
 let win = 0;
 let loss = 0;
 let winLoss = "";
 let countX = -1;
-let hisHight = 0;
 
 const aiClasses = [
-    "human-choice-rock",
-    "human-choice-paper",
-    "human-choice-scissors"
+  "human-choice-rock",
+  "human-choice-paper",
+  "human-choice-scissors",
 ];
 
-const winColor = [
-    winColor0,
-    winColor1,
-    winColor2,
-    winColor3,
-    winColor4
-];
+const winColor = [winColor0, winColor1, winColor2, winColor3, winColor4];
 
+const winOrLoss = [winOrLoss0, winOrLoss1, winOrLoss2, winOrLoss3, winOrLoss4];
 
-const winOrLoss = [
-    winOrLoss0,
-    winOrLoss1,
-    winOrLoss2,
-    winOrLoss3,
-    winOrLoss4
-];
+const yourImg = [yourImg0, yourImg1, yourImg2, yourImg3, yourImg4];
 
-const yourImg = [
-    yourImg0,
-    yourImg1,
-    yourImg2,
-    yourImg3,
-    yourImg4
-];
-
-const aiImg = [
-    aiImg0,
-    aiImg1,
-    aiImg2,
-    aiImg3,
-    aiImg4,
-];
+const aiImg = [aiImg0, aiImg1, aiImg2, aiImg3, aiImg4];
 
 // const scoreColor = [
 //     scoreColor0,
@@ -123,197 +89,180 @@ const aiImg = [
 //     scoreColor4
 // ];
 
-const bordHis = [
-    bordHis0,
-    bordHis1,
-    bordHis2,
-    bordHis3,
-    bordHis4,
-];
+const bordHis = [bordHis0, bordHis1, bordHis2, bordHis3, bordHis4];
 
-const box = [
-    box1,
-    box2,
-    box3,
-    box4,
-    box5
-];
- 
+const box = [box1, box2, box3, box4, box5];
 
 function randomNum() {
-    return Math.floor(Math.random() * 3) + 1;
+  return Math.floor(Math.random() * 3) + 1;
 }
 
 function checkResult(player, ai) {
+  if (player === ai) {
+    lastRound++;
+    winLoss = "draw";
+  } else if (
+    (player === 1 && ai === 3) ||
+    (player === 2 && ai === 1) ||
+    (player === 3 && ai === 2)
+  ) {
+    win++;
+    countX++;
+    humanCount.innerText = win;
+    winLoss = "win";
+  } else {
+    loss++;
+    countX++;
+    aiCount.innerText = loss;
+    winLoss = "loss";
+  }
 
-    if (player === ai) {
-        lastRound++;
-        winLoss = "draw";
-
-    } else if (
-        (player === 1 && ai === 3) ||
-        (player === 2 && ai === 1) ||
-        (player === 3 && ai === 2)
-        ) {
-
-        win++;
-        countX++;
-        humanCount.innerText = win;
-        winLoss = "win";
-        
-
-    } else {
- 
-        loss++;
-        countX++;
-        aiCount.innerText = loss;
-        winLoss = "loss";
-
-    };
-    console.log(winLoss);
-
-
-    if (winLoss === "win") {
-        hisMain.style.display = "flex";
-
-        winColor[countX].style.backgroundColor = "#00FF9D";
-        winOrLoss[countX].innerText = "WIN";
-        bordHis[countX].style.backgroundColor = "#00FF9D";
-        bordHis[countX].innerText = "+1";
-        box[countX].style.display = "flex";
-
-        hisHight += 53;
-        hisMain.style.height = `${hisHight}px`;
-       
-    } else if (winLoss === "loss") {
-        hisMain.style.display = "flex";
-
-        winColor[countX].style.backgroundColor = "#FF5C7A";
-        winOrLoss[countX].innerText = "LOSS";
-        bordHis[countX].style.backgroundColor = "#FF5C7A";
-        bordHis[countX].innerText = "-1";
-        box[countX].style.display = "flex";
-
-        hisHight += 53;
-        hisMain.style.height = `${hisHight}px`;
-    };
-
-    
-
- 
-
-
-};
-
-
+  if (winLoss === "win") {
+    winColor[countX].style.backgroundColor = "#00FF9D";
+    winOrLoss[countX].innerText = "WIN";
+    bordHis[countX].style.backgroundColor = "#00FF9D";
+    bordHis[countX].innerText = "+1";
+    box[countX].style.display = "flex";
+  } else if (winLoss === "loss") {
+    winColor[countX].style.backgroundColor = "#FF5C7A";
+    winOrLoss[countX].innerText = "LOSS";
+    bordHis[countX].style.backgroundColor = "#FF5C7A";
+    bordHis[countX].innerText = "-1";
+    box[countX].style.display = "flex";
+  }
+}
 
 function playGame(player) {
-    if (gameRound >= lastRound) {
-        round.innerText = `GameOver`;
-        return;
-    }
+  if (gameRound >= lastRound) {
+    round.innerText = `GameOver`;
+    return;
+  }
 
-    gameRound++;
+  gameRound++;
 
-    round.innerText = `Round ${gameRound}`;
+  round.innerText = `Round ${gameRound}`;
 
-    let ai = randomNum();
+  let ai = randomNum();
 
-    console.log(ai);
-    spinAnimation(ai);
+  console.log(ai);
+  spinAnimation(ai);
 
+  setTimeout(() => {
     checkResult(player, ai);
 
-    if (player===1) {
-        ChoiceImage.classList.remove("human-choice-scissors");
-        ChoiceImage.classList.remove("human-choice-paper");
-        ChoiceImage.classList.add("human-choice-rock");
-
-        yourImg[countX].classList.add("human-choice-rock");
- 
-    } else if (player===2) {
-        ChoiceImage.classList.remove("human-choice-rock");
-        ChoiceImage.classList.remove("human-choice-scissors");
-        ChoiceImage.classList.add("human-choice-paper");
-
-        yourImg[countX].classList.add("human-choice-paper");
-    } else {
-        ChoiceImage.classList.remove("human-choice-rock");
-        ChoiceImage.classList.remove("human-choice-paper");
-        ChoiceImage.classList.add("human-choice-scissors");
-
-        yourImg[countX].classList.add("human-choice-scissors");
+    if (winLoss === "draw") {
+      return;
     }
 
-    if (ai===1) {
-        aiImg[countX].classList.add("human-choice-rock");
-    } else if (ai===2) {
-        aiImg[countX].classList.add("human-choice-rock");
+    if (player === 1) {
+      ChoiceImage.classList.remove(
+        "human-choice-scissors",
+        "human-choice-paper",
+      );
+
+      ChoiceImage.classList.add("human-choice-rock");
+
+      yourImg[countX].classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors",
+      );
+
+      yourImg[countX].classList.add("human-choice-rock");
+    } else if (player === 2) {
+      ChoiceImage.classList.remove("human-choice-rock");
+      ChoiceImage.classList.remove("human-choice-scissors");
+      ChoiceImage.classList.add("human-choice-paper");
+
+      yourImg[countX].classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors",
+      );
+
+      yourImg[countX].classList.add("human-choice-paper");
     } else {
-        aiImg[countX].classList.add("human-choice-scissors");
+      ChoiceImage.classList.remove("human-choice-rock");
+      ChoiceImage.classList.remove("human-choice-paper");
+      ChoiceImage.classList.add("human-choice-scissors");
+
+      yourImg[countX].classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors",
+      );
+
+      yourImg[countX].classList.add("human-choice-scissors");
     }
-} 
+
+    if (ai === 1) {
+      aiImg[countX].classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors",
+      );
+
+      aiImg[countX].classList.add("human-choice-rock");
+    } else if (ai === 2) {
+      aiImg[countX].classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors",
+      );
+
+      aiImg[countX].classList.add("human-choice-paper");
+    } else {
+      aiImg[countX].classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors",
+      );
+
+      aiImg[countX].classList.add("human-choice-scissors");
+    }
+  }, 1500);
+}
 
 rock.addEventListener("click", () => {
-    playGame(1);
+  playGame(1);
 });
 
 paper.addEventListener("click", () => {
-    playGame(2);
+  playGame(2);
 });
 
 scissors.addEventListener("click", () => {
-    playGame(3);
+  playGame(3);
 });
 
-
-
-
-
-
-
-
-
-
-
-
 function spinAnimation(finalChoice) {
+  let index = 0;
 
-    let index = 0;
+  let spin = setInterval(() => {
+    aiChoiceImage.classList.remove(
+      "human-choice-rock",
+      "human-choice-paper",
+      "human-choice-scissors",
+    );
 
-    let spin = setInterval(() => {
+    aiChoiceImage.classList.add(aiClasses[index]);
 
-        aiChoiceImage.classList.remove(
-            "human-choice-rock",
-            "human-choice-paper",
-            "human-choice-scissors"
-        );
+    index++;
 
-        aiChoiceImage.classList.add(aiClasses[index]);
+    if (index >= aiClasses.length) {
+      index = 0;
+    }
+  }, 100);
 
-        index++;
+  setTimeout(() => {
+    clearInterval(spin);
 
-        if (index >= aiClasses.length) {
-            index = 0;
-        }
+    aiChoiceImage.classList.remove(
+      "human-choice-rock",
+      "human-choice-paper",
+      "human-choice-scissors",
+    );
 
-    }, 100);
-
-
-
-    setTimeout(() => {
-
-        clearInterval(spin);
-
-        aiChoiceImage.classList.remove(
-            "human-choice-rock",
-            "human-choice-paper",
-            "human-choice-scissors"
-        );
-
-        aiChoiceImage.classList.add(aiClasses[finalChoice - 1]);
-
-    }, 1500);
-
-}
-
+    aiChoiceImage.classList.add(aiClasses[finalChoice - 1]);
+  }, 1500);
+};

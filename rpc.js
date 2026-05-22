63b7b04bbb50088cc4a2058b5cb8eda1325cc6ab
gaming-cbu <siburamnayak18@.com>
2026-05-22 20:38:35 +0530

    Upgrade Some Problem

--- rpc.js
+++ rpc.js
@@ -15,46 +15,17 @@ let aiChoiceImage = document.querySelector("#cir-comp-x");
 
 let hisMain = document.querySelector(".history");
 
-let box1 = document.querySelector("#round-1");
-let box2 = document.querySelector("#round-2");
-let box3 = document.querySelector("#round-3");
-let box4 = document.querySelector("#round-4");
-let box5 = document.querySelector("#round-5");
+let box = document.querySelectorAll(".round-1");
 
-let winColor0 = document.querySelector(".bord-1-his");
-let winOrLoss0 = document.querySelector("#win-or-loss-1");
-let yourImg0 = document.querySelector(".img1-his-you");
-let aiImg0 = document.querySelector(".img-his-ai");
-let scoreColor0 = document.querySelector(".score-color");
-let bordHis0 = document.querySelector(".bord-1-hisX");
+let winColor = document.querySelectorAll(".bord-1-his");
+let winOrLoss = document.querySelectorAll(".win-or-loss-1");
+let yourImg = document.querySelectorAll(".img1-his-you");
+let aiImg = document.querySelectorAll(".img-his-ai");
+let scoreColor0 = document.querySelectorAll(".score-color");
+let bordHis = document.querySelectorAll(".bord-1-hisX");
 
-let winColor1 = document.querySelector(".bord-2-his");
-let winOrLoss1 = document.querySelector("#win-or-loss-2");
-let yourImg1 = document.querySelector(".img1-his-you-2");
-let aiImg1 = document.querySelector(".img-his-ai-2");
-let scoreColor1 = document.querySelector(".score-color-2");
-let bordHis1 = document.querySelector(".bord-2-hisX");
 
-let winColor2 = document.querySelector(".bord-3-his");
-let winOrLoss2 = document.querySelector("#win-or-loss-3");
-let yourImg2 = document.querySelector(".img1-his-you-3");
-let aiImg2 = document.querySelector(".img-his-ai-3");
-let scoreColor2 = document.querySelector(".score-color-3");
-let bordHis2 = document.querySelector(".bord-3-hisX");
 
-let winColor3 = document.querySelector(".bord-4-his");
-let winOrLoss3 = document.querySelector("#win-or-loss-4");
-let yourImg3 = document.querySelector(".img1-his-you-4");
-let aiImg3 = document.querySelector(".img-his-ai-4");
-let scoreColor3 = document.querySelector(".score-color-4");
-let bordHis3 = document.querySelector(".bord-4-hisX");
-
-let winColor4 = document.querySelector(".bord-5-his");
-let winOrLoss4 = document.querySelector("#win-or-loss-5");
-let yourImg4 = document.querySelector(".img1-his-you-5");
-let aiImg4 = document.querySelector(".img-his-ai-5");
-// let scoreColor4 = document.querySelector(".score-color-5");
-let bordHis4 = document.querySelector(".bord-5-hisX");
 
 // History End Here
 
@@ -63,12 +34,21 @@ let buttomText = document.querySelector("#made-button");
 let aiText = document.querySelector("#cir-comp-x-text");
 let yourText = document.querySelector("#cir-comp-y-text");
 
+let optionMain = document.querySelector(".option-main");
+let clickMain = document.querySelector(".cir-main");
+let winLossMain = document.querySelector("#win-loss-main");
+let nextBtn = document.querySelector(".play-next-button");
+let countDownText = document.querySelector("#count-down");
+
 let gameRound = 0;
 let lastRound = 5;
 let win = 0;
 let loss = 0;
 let winLoss = "";
 let countX = -1;
+let canClick = true;
+
+let thinkingAnimation;
 
 const aiClasses = [
   "human-choice-rock",
@@ -76,25 +56,6 @@ const aiClasses = [
   "human-choice-scissors",
 ];
 
-const winColor = [winColor0, winColor1, winColor2, winColor3, winColor4];
-
-const winOrLoss = [winOrLoss0, winOrLoss1, winOrLoss2, winOrLoss3, winOrLoss4];
-
-const yourImg = [yourImg0, yourImg1, yourImg2, yourImg3, yourImg4];
-
-const aiImg = [aiImg0, aiImg1, aiImg2, aiImg3, aiImg4];
-
-// const scoreColor = [
-//     scoreColor0,
-//     scoreColor1,
-//     scoreColor2,
-//     scoreColor3,
-//     scoreColor4
-// ];
-
-const bordHis = [bordHis0, bordHis1, bordHis2, bordHis3, bordHis4];
-
-const box = [box1, box2, box3, box4, box5];
 
 function randomNum() {
   return Math.floor(Math.random() * 3) + 1;
@@ -102,7 +63,8 @@ function randomNum() {
 
 function checkResult(player, ai) {
   if (player === ai) {
-    lastRound++;
+    // lastRound++;
+    gameRound--;
     winLoss = "draw";
   } else if (
     (player === 1 && ai === 3) ||
@@ -135,73 +97,91 @@ function checkResult(player, ai) {
   }
 }
 
+function setChoice(element, choice) {
+
+    element.classList.remove(
+        "human-choice-rock",
+        "human-choice-paper",
+        "human-choice-scissors"
+    );
+
+    element.classList.add(aiClasses[choice - 1]);
+}
+
 function playGame(player) {
-  if (gameRound >= lastRound) {
+  if (!canClick) return;
+  canClick = false;
+
+  if (gameRound > lastRound) {
     round.innerText = `GameOver`;
     return;
   }
 
-  gameRound++;
+  if (gameRound === 0) {
+    gameRound++;
 
-  round.innerText = `Round ${gameRound}`;
+    round.innerText = `Round ${gameRound}`;
+  }
+
+  
 
   let ai = randomNum();
 
   console.log(ai);
   spinAnimation(ai);
 
-  if (player=== 1) {
-    ChoiceImage.classList.remove(
-      "human-choice-scissors",
-      "human-choice-paper",
-    );
+  if (player === 1) {
 
-    ChoiceImage.classList.add("human-choice-rock");
+    setChoice(ChoiceImage, player);
 
     yourText.innerText = "Rock";
-  }  else if (player === 2) {
-    ChoiceImage.classList.remove(
-      "human-choice-scissors",
-      "human-choice-rock",
-    );
+  } else if (player === 2) {
 
-    ChoiceImage.classList.add("human-choice-paper");
+    setChoice(ChoiceImage, player);
+
     yourText.innerText = "Paper";
   } else {
-    ChoiceImage.classList.remove(
-      "human-choice-paper",
-      "human-choice-rock",
-    );
+    
+   setChoice(ChoiceImage, player);
 
-    ChoiceImage.classList.add("human-choice-scissors");
     yourText.innerText = "Scissors";
   }
 
+  let countXX = 0;
 
-  let countXX = 1;
-
-  let thinkingAnimation = setInterval(() => {
-
+  thinkingAnimation = setInterval(() => {
     aiText.innerText = "AI is thinking" + ".".repeat(countXX);
 
-     countXX++;
+    countXX++;
 
     if (countXX > 3) {
-      countXX = 1;
-   }
-
-  }, 500);
-
-
-
-
+      countXX = 0;
+    }
+  }, 200);
 
   setTimeout(() => {
+    
+
+    clickMain.style.display = "none";
+    optionMain.style.display = "flex";
+
+    clearInterval(thinkingAnimation);
+
     checkResult(player, ai);
 
-    // if (winLoss === "draw") {
-    //   return;
-    // }
+    if (winLoss === "win") {
+      winLossMain.innerText = "VICTORY";
+      console.log("Win");
+    } else if (winLoss === "loss") {
+      winLossMain.innerText = "DEFEAT";
+      console.log("Loss");
+    } else {
+      winLossMain.innerText = "DRAW";
+    }
+
+   
+    
+
 
     if (player === 1) {
       yourImg[countX].classList.remove(
@@ -220,7 +200,6 @@ function playGame(player) {
 
       yourImg[countX].classList.add("human-choice-paper");
     } else {
-
       yourImg[countX].classList.remove(
         "human-choice-rock",
         "human-choice-paper",
@@ -228,7 +207,6 @@ function playGame(player) {
       );
 
       yourImg[countX].classList.add("human-choice-scissors");
-
     }
 
     if (ai === 1) {
@@ -241,7 +219,6 @@ function playGame(player) {
       aiImg[countX].classList.add("human-choice-rock");
 
       aiText.innerText = "Rock";
-      
     } else if (ai === 2) {
       aiImg[countX].classList.remove(
         "human-choice-rock",
@@ -252,7 +229,6 @@ function playGame(player) {
       aiImg[countX].classList.add("human-choice-paper");
 
       aiText.innerText = "Paper";
-
     } else {
       aiImg[countX].classList.remove(
         "human-choice-rock",
@@ -264,8 +240,20 @@ function playGame(player) {
 
       aiText.innerText = "Scissors";
     }
+
+    canClick = true;
   }, 1500);
 }
+
+
+nextBtn.addEventListener("click", () => {
+  clickMain.style.display = "flex";
+  optionMain.style.display = "none";
+
+  gameRound++;
+  round.innerText = `Round ${gameRound}`;
+});
+
 
 rock.addEventListener("click", () => {
   playGame(1);
@@ -309,4 +297,4 @@ function spinAnimation(finalChoice) {
 
     aiChoiceImage.classList.add(aiClasses[finalChoice - 1]);
   }, 1500);
-};
+}
